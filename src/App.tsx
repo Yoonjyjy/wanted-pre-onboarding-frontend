aa28f38216e1881753785767fd526f5b47ef4ba6
Yoonjyjy <diniless@naver.com>
2023-06-21 17:04:44 +0900

    초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import SignIn from "pages/Signin";
 
 function App() {
+  // let isAuthorized = sessionStorage.getItem("isAuthorized");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/signin"></Navigate>}></Route>
+      <Route path="/signin" element={<SignIn />} />
+    </Routes>
+
+    // <div>{!isAuthorized ? <Redirect to="/login" /> : <Redirect to="/" />}</div>
   );
 }
 
